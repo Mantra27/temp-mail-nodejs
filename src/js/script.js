7d91818cbed1a9569bccfcb91f37e5075df791e4
Mantra <75286433+Mantra27@users.noreply.github.com>
2021-11-17 14:18:16 +0530

    Typo

--- src/js/script.js
+++ src/js/script.js
@@ -45,7 +45,7 @@ document.getElementById("btn").addEventListener("click", () => {
       document.getElementById("mail").value = data.data;
       console.log("Success:", data);
       document.getElementById("info").innerText =
-        "New Emails on this address will be displayed here.";
+        "New Emails on this address will be displayed here(no need to refresh)";
       document.getElementById("btn").innerText = "Mailbox(0 new mails);";
       return data.data.split("@");
     })
